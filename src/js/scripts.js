738f69179beea082fc5b38b461dd059dba3b979a
Audrey Wong <audw@bu.edu>
2021-11-21 15:54:31 -0500

    Cookie Builder Step 1

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -1,1 +1,14 @@
 // Add your scripts here
+<<<<<<< Updated upstream
+=======
+function myFunction() {
+  const x = document.getElementById('topnav');
+  if (x.className === 'nav') {
+    x.className += 'responsive';
+  } else {
+    x.className = 'nav';
+  }
+}
+
+document.getElementById('hamburger').addEventListener(onclick, myFunction);
+>>>>>>> Stashed changes
